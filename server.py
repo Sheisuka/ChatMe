ac4d8b85753256590923d784f9ef2868188fb9a2
sheisuka <xxtornex@gmail.com>
2023-02-01 19:07:35 +0700

    select server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import select
 
 
 class Server:
@@ -7,20 +8,41 @@ class Server:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
+        self.inputs, self.outputs = [self.sock], []
+
     
     def setup(self, host, port, max_listen):
         self.sock.bind((host, port))
         self.sock.listen(max_listen)
+    
+    def handle(self, sock, addr):
+        try:
+            data = sock.recv(1024)
+        except ConnectionError:
+            print(f"Client suddenly closed while receiving")
+            return False
+        print(f"Received from {addr}:\n\t{data}")
+        if not data:
+            print(f"Dicsonnected {addr}")
+            return False
 
     def receive(self):
-        conn, address = self.sock.accept()
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            print(f"Get from {address[0]}:\n\t{data}")
-        conn.close()
+        readable, writeable, exceptional = select.select(self.inputs, self.outputs, self.inputs)
+        for sock in readable:
+            if sock == self.sock:
+                sock, addr = self.sock.accept()
+                print(f"Connected by {addr}")
+                self.inputs.append(sock)
+            else:
+                addr = self.sock.getpeername()
+                if not self.handle(sock, addr):
+                    self.inputs.remove(sock)
+                    if sock in self.outputs:
+                        self.outputs.remove(sock)
+                    sock.close()
+    
     
 server = Server()
-server.setup('127.0.0.1', 9000, 2)
-server.receive()
+server.setup('127.0.0.1', 9001, 2)
+while True:
+    server.receive()
