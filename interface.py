b48b192a6562ffbeb900e7d300ec03259982a329
sheisuka <xxtornex@gmail.com>
2023-02-08 23:08:25 +0700

    Refactoring
    No more Interface class, each view is standalone class

--- interface.py
+++ interface.py
@@ -1,11 +1,51 @@
 import urwid
 
 
-class Interface:
-    def __init__(self) -> None: 
-        pass
     
-    def get_intro(self) -> urwid.Filler:
+def exit_program() -> None:
+    raise urwid.ExitMainLoop()
+    
+
+class MenuElement(urwid.Text):
+    def __init__(self, caption: str) -> None:
+        urwid.Text.__init__(self, caption)
+        urwid.register_signal(self.__class__, ['activate'])
+
+    def keypress(self, size, key):
+        if key == 'enter':
+            urwid.emit_signal(self, 'activate')
+        else:
+            return key
+    
+    def selectable(self):
+        return True
+
+
+class MainMenu(urwid.ListBox):
+    def __init__(self):
+        body = [urwid.Text("What you want to do?"), urwid.Divider()]
+
+        create_button = MenuElement("Create Room")
+        join_button = MenuElement("Join Room")
+        exit_button = MenuElement("Exit")
+
+        #urwid.connect_signal(create_button, 'activate', self.create_room)
+        #urwid.connect_signal(join_button, 'activate', self.join_room)
+        #urwid.connect_signal(exit_button, 'activate', self.exit_program)
+
+        body.append(urwid.Padding(urwid.AttrMap(create_button, 'button.normal', 
+                                                'button.focus'), align='center'))
+        body.append(urwid.Padding(urwid.AttrMap(join_button, 'button.normal', 
+                                                'button.focus'), align='center'))
+        body.append(urwid.Padding(urwid.AttrMap(exit_button, 'button.normal', 
+                                                'button.focus'), align='center'))
+
+        urwid.ListBox.__init__(self, urwid.SimpleFocusListWalker(body))
+    
+
+
+class Introduction(urwid.Filler):
+    def __init__(self):
         bt = urwid.BigText("ChatMe", urwid.HalfBlock7x7Font())
         bt = urwid.Padding(bt, "center", None)
 
@@ -17,47 +57,26 @@ class Interface:
             urwid.Divider(bottom=2),
             urwid.Text("Created by sheisuka", align="center"),
         ])
-        return urwid.Filler(intro)
 
-    def get_menu(self, key) -> urwid.Filler:
-        body = [urwid.Text("What you want to do?"), urwid.Divider()]
-
-        create_button = urwid.Button("Create Room")
-        join_button = urwid.Button("Join Room")
-        exit_button = urwid.Button("Exit")
-
-        urwid.connect_signal(create_button, 'click', self.create_room)
-        urwid.connect_signal(join_button, 'click', self.join_room)
-        urwid.connect_signal(exit_button, 'click', self.exit_program)
-
-        body.append(urwid.AttrMap(create_button, None, focus_map='reversed'))
-        body.append(urwid.AttrMap(join_button, None, focus_map='reversed'))
-        body.append(urwid.AttrMap(exit_button, None, focus_map='reversed'))
-
-        self.loop.widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+        urwid.Filler.__init__(self, intro)
+        urwid.register_signal(self.__class__, ['activate'])
+        urwid.connect_signal(self, 'activate', go_to_menu)
     
-    def create_room(self, key):
-        print('server')
-
-    def get_join_room(self):
-        ask = urwid.Edit(("I say", "Enter ip and port.\n For example 127.0.0.1 9000.\n"))
-        reply = urwid.Text("")
-        back_button = urwid.Button("Back")
-        pile = urwid.Pile([ask, urwid.Divider(), reply, urwid.Divider(), back_button])
-        filler = urwid.Filler(pile)
-        return filler
-
-    def join_room(self, key):
-        join_room_filler = self.get_join_room()
-        self.loop.widget = join_room_filler
+    def keypress(self, size, key):
+        urwid.emit_signal(self, 'activate')
     
-    def exit_program(self, key) -> None:
-        raise urwid.ExitMainLoop()
+    def selectable(self):
+        return True
 
-    def run(self) -> None:
-        filler = self.get_intro()
-        self.loop = urwid.MainLoop(filler, unhandled_input=self.get_menu)
-        self.loop.run()
-    
-inter = Interface()
-inter.run()
+
+def go_to_menu():
+    window = urwid.LineBox(MainMenu())
+    topw = urwid.Overlay(window, background, 'center', 30, 'middle', 10)
+    mainloop.widget = topw
+
+some_palette = [('basic', 'yellow', 'dark blue'), ('button.normal', 'white', 'black'), ('button.focus', 'yellow', 'light blue')]
+window = urwid.LineBox(Introduction())
+background = urwid.SolidFill()
+topw = urwid.Overlay(window, background, 'center', 100, 'middle', 40)
+mainloop = urwid.MainLoop(topw, palette=some_palette)
+mainloop.run()
