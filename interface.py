4db9f3fbdaa1b66167a69312990647a08a5ca6c8
sheisuka <xxtornex@gmail.com>
2023-02-10 21:11:19 +0700

    Add PreJoinView where user enters address and tries to connect

--- interface.py
+++ interface.py
@@ -1,28 +1,56 @@
 import urwid
 
 
-    
-def exit_program() -> None:
-    raise urwid.ExitMainLoop()
-    
-
 class MenuElement(urwid.Text):
     def __init__(self, caption: str) -> None:
         urwid.Text.__init__(self, caption)
         urwid.register_signal(self.__class__, ['activate'])
 
-    def keypress(self, size, key):
+    def keypress(self, size, key) -> None | str:
         if key == 'enter':
             urwid.emit_signal(self, 'activate')
         else:
             return key
     
-    def selectable(self):
+    def selectable(self) -> bool:
         return True
+    
 
+class PreJoinView(urwid.Filler):
+    def __init__(self) -> None:
+        div = urwid.Divider()
+
+        caption = urwid.Text(('caption', 'Enter ip and port (ip:port)'), align='center')
+        input = urwid.Edit(multiline=False, align='center')
+
+        connect_button = urwid.Button('Connect')
+        back_button = urwid.Button('Back')
+
+        urwid.connect_signal(back_button, 'click', go_to_menu)
+        urwid.connect_signal(connect_button, 'click', self.connect_chat, input)
+        back_button_wrap = urwid.Padding(urwid.AttrMap(back_button, 'button.normal', 'button.focus'),
+                                            align='center', width=8)
+        connect_button_wrap = urwid.Padding(urwid.AttrMap(connect_button, 'button.normal', 'button.focus'),
+                                            align='center', width=11)
+        body = urwid.Pile([caption, div, input, div, connect_button_wrap, back_button_wrap])
+        urwid.Filler.__init__(self, body)
+    
+    def connect_chat(self, key, input) -> None:
+        address = input.text
+        ip, port = address.split(':')
+        #self.address_correct(ip, port)
+    
+    def address_correct(ip, port) -> None:
+        for number in ip.split('.'):
+            if int(number) > 255:
+                # raise IncorrectIPException
+                ...
+        if int(port) > 65535:
+            # IncorrectPortException
+            ...
 
 class MainMenu(urwid.ListBox):
-    def __init__(self):
+    def __init__(self) -> None:
         body = [urwid.Text("What you want to do?"), urwid.Divider()]
 
         create_button = MenuElement("Create Room")
@@ -30,8 +58,8 @@ class MainMenu(urwid.ListBox):
         exit_button = MenuElement("Exit")
 
         #urwid.connect_signal(create_button, 'activate', self.create_room)
-        #urwid.connect_signal(join_button, 'activate', self.join_room)
-        #urwid.connect_signal(exit_button, 'activate', self.exit_program)
+        urwid.connect_signal(join_button, 'activate', go_to_join)
+        urwid.connect_signal(exit_button, 'activate', exit_program)
 
         body.append(urwid.Padding(urwid.AttrMap(create_button, 'button.normal', 
                                                 'button.focus'), align='center'))
@@ -45,7 +73,7 @@ class MainMenu(urwid.ListBox):
 
 
 class Introduction(urwid.Filler):
-    def __init__(self):
+    def __init__(self) -> None:
         bt = urwid.BigText("ChatMe", urwid.HalfBlock7x7Font())
         bt = urwid.Padding(bt, "center", None)
 
@@ -62,17 +90,28 @@ class Introduction(urwid.Filler):
         urwid.register_signal(self.__class__, ['activate'])
         urwid.connect_signal(self, 'activate', go_to_menu)
     
-    def keypress(self, size, key):
+    def keypress(self, size, key) -> None:
         urwid.emit_signal(self, 'activate')
     
-    def selectable(self):
+    def selectable(self) -> bool:
         return True
 
 
-def go_to_menu():
+def exit_program() -> None:
+    raise urwid.ExitMainLoop()
+
+
+def go_to_menu(*args) -> None:
     window = urwid.LineBox(MainMenu())
     topw = urwid.Overlay(window, background, 'center', 30, 'middle', 10)
     mainloop.widget = topw
+
+
+def go_to_join(*args) -> None:
+    window = urwid.LineBox(PreJoinView())
+    topw = urwid.Overlay(window, background, 'center', 30, 'middle', 10)
+    mainloop.widget = topw
+
 
 some_palette = [('basic', 'yellow', 'dark blue'), ('button.normal', 'white', 'black'), ('button.focus', 'yellow', 'light blue')]
 window = urwid.LineBox(Introduction())
