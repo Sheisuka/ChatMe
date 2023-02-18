981a2cc51a35e52966b06ec58b8f5292f1517474
sheisuka <xxtornex@gmail.com>
2023-02-18 17:38:50 +0700

    add test connection with sig_server

--- server.py
+++ server.py
@@ -1,18 +1,35 @@
-import selectors
 import socket
 import asyncio
 from typing import Tuple
+import stun
 # import logging
 
 class Server:
     def __init__(self) -> None:
         self.serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #self.logger = logging.getLogger(__name__)
-        self.chunk_size = 1024
-        self.host = '127.0.0.1'
-        self.port = 9002
+
+        self.name = 'test_server'
+
+        self.sig_server_host = ''
+        self.sig_server_port = 0
+
+        self.in_host = '127.0.0.1'
+        self.in_port = 9000
+
+        self.out_host = ''
+        self.out_port = 0
+
         self.max_listeners = 2
         self.set_blocking_flag = False
+
+    def set_out_address(self):
+        nat_type, self.out_host, self.out_port = stun.get_ip_info()
+        print(nat_type, self.out_host, self.out_port)
+    
+    def connect_to_sig(self):
+        data = f"{self.in_host}|{self.in_port}|{self.out_host}|{self.out_port}|{self.name}"
+        self.serv_sock.sendto(data, (self.sig_server_host, self.sig_server_port))
 
     async def handle_connection(self, sock: socket, addr: Tuple[str, int]):
         loop = asyncio.get_event_loop()
@@ -32,15 +49,16 @@ class Server:
     async def main(self) -> None:
         self.setup()
         print("Server started") # replace with logger + send message to creator
-
-        loop = asyncio.get_event_loop()
-        while True:
-            print("Waiting for connection") # replace with logger + send message to creator
-            sock, addr = await loop.sock_accept(self.serv_sock)
-            loop.create_task(self.handle_connection(sock, addr))
+        self.set_out_address()
+        self.connect_to_sig()
+        #loop = asyncio.get_event_loop()
+        #while True:
+        #    print("Waiting for connection") # replace with logger + send message to creator
+        #    sock, addr = await loop.sock_accept(self.serv_sock)
+        #    loop.create_task(self.handle_connection(sock, addr))
     
     def setup(self) -> None:
-        self.serv_sock.bind((self.host, self.port))
+        self.serv_sock.bind((self.in_host, self.in_port))
         self.serv_sock.listen(self.max_listeners)
         self.serv_sock.setblocking(self.set_blocking_flag)
 
